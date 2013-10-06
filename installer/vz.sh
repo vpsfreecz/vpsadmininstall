b30548284f95bf186f44713ed634dd7ca759d450
Jakub Skokan <jakub.skokan@havefun.cz>
2013-10-06 16:57:09 +0200

    Disable vzquota on ZFS nodes

--- installer/vz.sh
+++ installer/vz.sh
@@ -8,6 +8,13 @@ run rpm --import http://ftp.openvz.org/RPM-GPG-Key-OpenVZ
 msg "Installing vzkernel and vzctl"
 run yum -y install vzkernel vzctl
 
+title "Configuring OpenVZ..."
+
+if [ "$NODE_FSTYPE" == "zfs" ] || [ "$NODE_FSTYPE" == "zfs_compat" ] ; then
+	msg "Disabling vzquota"
+	run sed -i 's/^DISK_QUOTA=yes$/DISK_QUOTA=no/' /etc/vz/vz.conf
+fi
+
 echo "$BASEDIR/installer/resume.sh \"$BASEDIR\"" >> /etc/rc.d/rc.local
 
 echo ""

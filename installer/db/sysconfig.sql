93e908184d569804755da57c507600b6a2b80210
Jakub Skokan <jakub.skokan@havefun.cz>
2013-10-07 18:31:39 +0200

    Clean SQL scripts

--- installer/db/sysconfig.sql
+++ installer/db/sysconfig.sql
@@ -1,12 +1,3 @@
-SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
-SET time_zone = "+00:00";
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
-
-
 INSERT INTO `sysconfig` (`cfg_name`, `cfg_value`) VALUES
 ('db_version', '"install"'),
 ('default_config_chain', '["27","28","6","22"]'),
@@ -17,7 +8,3 @@ INSERT INTO `sysconfig` (`cfg_name`, `cfg_value`) VALUES
 ('playground_default_config_chain', '["27","28","6","17"]'),
 ('playground_enabled', 'false'),
 ('playground_vps_lifetime', '30');
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;

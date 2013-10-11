99b4a635bac022dd911b4d3e1b10b3a7d316e332
Jakub Skokan <jakub.skokan@havefun.cz>
2013-10-11 17:08:21 +0200

    Silence yum

--- install_node.sh
+++ install_node.sh
@@ -6,12 +6,12 @@ which git > /dev/null 2>&1
 
 if [ "$?" != "0" ] ; then
 	echo "* Installing git..."
-	yum -y -q install git
+	yum -y -q install git >> /dev/null
 	echo ""
 fi
 
 if [ "$VPSADMIN_CLONE" != "no" ] ; then
-	git clone git://git.vpsfree.cz/vpsadmininstall.git "$TARGET"
+	git clone git://git.vpsfree.cz/vpsadmininstall.git "$TARGET" >> /dev/null
 fi
 
 cd "$TARGET"
